26031c29d7f3849624a78cac41611e6ab4751387
edenave <76529301+edenave@users.noreply.github.com>
2021-01-22 18:14:43 +0200

    find totaltime and precentage of tasks finished

---
+++ script.js
@@ -0,0 +1,153 @@
+"use strict"
+
+const mainArr = [
+    {
+
+        startedAt: new Date("2021-01-20:13:00"),
+        
+        finishedAt: new Date("2021-01-20:19:00"),
+        
+        tasksGiven: 10,
+        
+        tasksFinished: 7,
+        
+        topic: "JavaScript"
+        
+        },
+
+
+        {
+
+            startedAt: new Date("2021-01-20:11:00"),
+            
+            finishedAt: new Date("2021-01-20:14:00"),
+            
+            tasksGiven: 10,
+            
+            tasksFinished: 7,
+            
+            topic: "JavaScript"
+            
+            },
+
+            {
+
+                startedAt: new Date("2021-01-20:11:00"),
+                
+                finishedAt: new Date("2021-01-20:14:00"),
+                
+                tasksGiven: 10,
+                
+                tasksFinished: 7,
+                
+                topic: "JavaScript"
+                
+                },
+                {
+
+                    startedAt: new Date("2021-01-20:11:00"),
+                    
+                    finishedAt: new Date("2021-01-20:14:00"),
+                    
+                    tasksGiven: 10,
+                    
+                    tasksFinished: 7,
+                    
+                    topic: "JavaScript"
+                    
+                    },
+                    {
+
+                        startedAt: new Date("2021-01-20:11:00"),
+                        
+                        finishedAt: new Date("2021-01-20:14:00"),
+                        
+                        tasksGiven: 10,
+                        
+                        tasksFinished: 7,
+                        
+                        topic: "JavaScript"
+                        
+                        },
+                        {
+
+                            startedAt: new Date("2021-01-20:11:00"),
+                            
+                            finishedAt: new Date("2021-01-20:14:00"),
+                            
+                            tasksGiven: 10,
+                            
+                            tasksFinished: 7,
+                            
+                            topic: "JavaScript"
+                            
+                            },
+                            {
+
+                                startedAt: new Date("2021-01-20:11:00"),
+                                
+                                finishedAt: new Date("2021-01-20:14:00"),
+                                
+                                tasksGiven: 10,
+                                
+                                tasksFinished: 7,
+                                
+                                topic: "JavaScript"
+                                
+                                },
+                                {
+
+                                    startedAt: new Date("2021-01-20:11:00"),
+                                    
+                                    finishedAt: new Date("2021-01-20:14:00"),
+                                    
+                                    tasksGiven: 10,
+                                    
+                                    tasksFinished: 7,
+                                    
+                                    topic: "JavaScript"
+                                    
+                                    },
+                                    {
+
+                                        startedAt: new Date("2021-01-20:11:00"),
+                                        
+                                        finishedAt: new Date("2021-01-20:14:00"),
+                                        
+                                        tasksGiven: 10,
+                                        
+                                        tasksFinished: 7,
+                                        
+                                        topic: "JavaScript"
+                                        
+                                        },
+                                        {
+
+                                            startedAt: new Date("2021-01-20:11:00"),
+                                            
+                                            finishedAt: new Date("2021-01-20:14:00"),
+                                            
+                                            tasksGiven: 10,
+                                            
+                                            tasksFinished: 7,
+                                            
+                                            topic: "JavaScript"
+                                            
+                                            },
+
+];
+
+let totalTime;
+
+for( let x of mainArr){
+x.totalTime = (x.finishedAt - x.startedAt)/3600000 + " hours";
+}
+
+for(let y of mainArr ){
+    y.tasksFinishedPrecent = (y.tasksFinished / y.tasksGiven) * 100 + " %";
+}
+
+
+
+
+console.log(mainArr)
