cd46c299753d9b8cbc4d55749a9fc193ade4b78c
edenave <76529301+edenave@users.noreply.github.com>
2021-01-22 23:51:01 +0200

    add object.keys for table head

--- script.js
+++ script.js
@@ -141,15 +141,34 @@ let totalTime;
 
 for( let x of mainArr){
 x.totalTime = (x.finishedAt - x.startedAt)/3600000 + " hours";
-}
+};
 
 for(let y of mainArr ){
 
     let yfloored = Math.floor((y.tasksFinished / y.tasksGiven) * 100)
     y.tasksFinishedPrecent = yfloored + " %";
-}
+
+
+    
+};
 
 
 
 
-console.log(mainArr)
+let tableStart = document.write("<table>");
+// let table head = document.write(`<th> <td>${Object.keys(mainArr)} </td></th>`)
+// for(let th of mainArr){
+   let tableHead = document.write(`<th> ${Object.keys(mainArr[1])}</th>`)
+// }
+
+for (let tr of mainArr){
+    document.write( "<tr><td>" + tr + "</td></tr>")
+};
+
+
+
+let tableEnd = document.write("</table>");
+// console.log(mainArr)
+
+
+
